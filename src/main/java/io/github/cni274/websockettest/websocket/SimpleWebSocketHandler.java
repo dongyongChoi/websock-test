197c5c18e8e3174a43de261c3f25fd56d0f2a3c8
ChoiDongYong <cni274@gmail.com>
2025-05-24 11:57:13 +0900

    Simple Socket 동작 코드 작성

---
+++ src/main/java/io/github/cni274/websockettest/websocket/SimpleWebSocketHandler.java
@@ -0,0 +1,31 @@
+package io.github.cni274.websockettest.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class SimpleWebSocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            if (!s.isOpen()) {
+                continue;
+            }
+
+            s.sendMessage(new TextMessage("hello : " + message.getPayload()));
+        }
+    }
+}
---
+++ src/main/java/io/github/cni274/websockettest/websocket/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package io.github.cni274.websockettest.websocket;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final SimpleWebSocketHandler simpleWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(simpleWebSocketHandler, "/ws-chat").setAllowedOrigins("*");
+    }
+}
